a7e25cc5e42966cd2cd80486f84c7b75611c87de
Michał Tkacz <58853722+michaltkacz@users.noreply.github.com>
2021-04-02 15:57:51 +0200

    Added combobox for categories

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,7 +1,10 @@
-import React from "react";
-import { Form, InputGroup, Button, FormControl } from "react-bootstrap";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+import { Form, InputGroup, Button, FormControl } from 'react-bootstrap';
+
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const ExpenseForm = ({
   submitHandler,
@@ -13,6 +16,19 @@ const ExpenseForm = ({
   amountInput,
   setAmountInput,
 }) => {
+  const [availableCategories, setAvailableCategories] = useState(null);
+
+  useEffect(async () => {
+    // potrzebny link
+    fetch('https://webhomebudget.azurewebsites.net/api/category/base')
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        setAvailableCategories(response);
+      });
+  }, []);
+
   return (
     <Form
       inline
@@ -20,34 +36,45 @@ const ExpenseForm = ({
         submitHandler(event);
       }}
     >
-      <InputGroup className="mb-2 mr-sm-2">
+      <InputGroup className='mb-2 mr-sm-2'>
         <Form.Control
+          as='select'
           value={categoryInput}
           onChange={(event) => {
             setCategoryInput(event.target.value);
           }}
-          className="mb-2 mr-sm-2"
-          id="inlineFormInputName2"
-          placeholder="Category"
-        />
+          className='mb-2 mr-sm-2'
+          id='inlineFormInputName2'
+          placeholder='Category'
+        >
+          {availableCategories ? (
+            availableCategories.map((category) => (
+              <option key={category.id} value={category.id}>
+                {category.name}
+              </option>
+            ))
+          ) : (
+            <option>unknown (temporary)</option>
+          )}
+        </Form.Control>
         <FormControl
           value={amountInput}
           onChange={(event) => {
             setAmountInput(event.target.value);
           }}
-          className="mb-2 mr-sm-2"
-          id="inlineFormInputGroupUsername2"
-          placeholder="Amount"
+          className='mb-2 mr-sm-2'
+          id='inlineFormInputGroupUsername2'
+          placeholder='Amount'
         />
         <DatePicker
-          className="form-control mb-2 mr-sm-2"
+          className='form-control mb-2 mr-sm-2'
           selected={startDate}
           onChange={(date) => setStartDate(date)}
         />
-        <Button type="submit" className="mb-2 mr-sm-2">
+        <Button type='submit' className='mb-2 mr-sm-2'>
           Submit
         </Button>
-        <Button type="submit" className="mb-2 mr-sm-2" onClick={cancelHandler}>
+        <Button type='submit' className='mb-2 mr-sm-2' onClick={cancelHandler}>
           Cancel
         </Button>
       </InputGroup>
