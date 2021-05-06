d1c2d3438ba2636643ffc7d05d365655ac0bc9d2
TGrochowski87 <t.grochowski99@gmail.com>
2021-05-06 12:00:48 +0200

    navbar misplaced

--- src/components/planning/planningNav/PlanningNav.js
+++ src/components/planning/planningNav/PlanningNav.js
@@ -12,33 +12,42 @@ const PlanningNav = ({ monthPlans }) => {
 
   return (
     <div
-      className="planning-nav"
+      className="planning-nav-space"
       style={
         scrollY > 66.357
           ? { position: "fixed", top: "0" }
           : { position: "absolute", top: "66.357px" }
       }
-      //style={{ position: window.scrollY < 66.357 ? "fixed" : "absolute" }}
     >
-      <div className="planning-nav-header">
-        <>
-          <h3>Expense planning</h3>
-        </>
-      </div>
-
-      {monthPlans.map((plan) => (
-        <PlanningNavItem key={plan.id} planId={plan.id} name={plan.monthName} />
-      ))}
       <div
-        className="new-plan"
-        onClick={() => {
-          //addMonthPlan();
-        }}
+        className="planning-nav"
+
+        //style={{ position: window.scrollY < 66.357 ? "fixed" : "absolute" }}
       >
-        <>
-          <h5>Plan another month</h5>
-          <AddIcon />
-        </>
+        <div className="planning-nav-header">
+          <>
+            <h3>Expense planning</h3>
+          </>
+        </div>
+
+        {monthPlans.map((plan) => (
+          <PlanningNavItem
+            key={plan.id}
+            planId={plan.id}
+            name={plan.monthName}
+          />
+        ))}
+        <div
+          className="new-plan"
+          onClick={() => {
+            //addMonthPlan();
+          }}
+        >
+          <>
+            <h5>Plan another month</h5>
+            <AddIcon />
+          </>
+        </div>
       </div>
     </div>
   );
